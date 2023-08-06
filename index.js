046bf9f11608d95ad7a7620fb70a9f9365d594bb
ritesh22201 <riteshgoswami22201@gmail.com>
2023-08-06 12:01:29 +0530

    code added

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middleware = jsonServer.defaults();
-const port = process.env.PORT || 5000;
+require('dotenv').config();
 
 server.use(middleware);
 server.use(router);
 
-server.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+server.listen(process.env.PORT, () => {
+    console.log(`Server is listening on port ${process.env.PORT}`);
 })
